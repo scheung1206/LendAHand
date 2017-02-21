49daba39cb9746815adc4b5ced0249e07d3799c4
scheung1206 <scheung1206@gmail.com>
2017-02-21 00:35:23 -0800

    Post Layout

--- client/app/postIndex/postIndex.controller.js
+++ client/app/postIndex/postIndex.controller.js
@@ -35,4 +35,24 @@ angular.module('codeApp')
       $scope.isStar = function(obj){
       return Auth.isLoggedIn() && obj && obj.stars && obj.stars.indexOf(Auth.getCurrentUser()._id)!==-1;
     };
+
+    $scope.star = function(obj) {
+      $http.put('/api/posts/' + obj._id  + '/star').success(function(){
+        loadPosts();
+      });
+    };
+    $scope.unstar = function(obj) {
+      $http.delete('/api/posts/' + obj._id  + '/star').success(function(){
+        loadPosts();
+      });
+    };
+    var loadPosts = function(){
+      $http.get('/api/posts', {params: {query: query}}).success(function(posts) {
+        $scope.posts = posts;
+        if($scope.posts.length < 20){
+          $scope.noMoreData = true;
+        }
+        $scope.busy = false;
+        });
+    };
   });
--- client/app/postShow/postShow.controller.js
+++ client/app/postShow/postShow.controller.js
@@ -53,4 +53,5 @@ angular.module('codeApp')
      });
    };
 
+
   });
