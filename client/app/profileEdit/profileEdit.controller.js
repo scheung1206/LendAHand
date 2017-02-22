62a8ede9fbbd52758881ebaa17677beb9389a07f
Douglas Tran <douglas.tran94@yahoo.com>
2017-02-21 17:19:00 -0800

    Revert "Revert "Profile V0.0.4""
    This reverts commit 7bde5d2eb1d8d4500263031783297ce38cf5ece2.

--- client/app/profileEdit/profileEdit.controller.js
+++ client/app/profileEdit/profileEdit.controller.js
@@ -1,10 +1,10 @@
-// 'use strict';
+'use strict';
 
-// angular.module('codeApp')
-//   .controller('ProfileEditCtrl', function ($scope, $http, $location) {
-// $scope.submitEdit = function(){
-// 	$http.post('/api/profile', $scope.profile).success(function(){
-// 	$location.path('/');	
-//   });
-// };
-// });
+angular.module('codeApp')
+  .controller('ProfileEditCtrl', function ($scope, $http, $location) {
+$scope.submitEdit = function(){
+	$http.post('/api/profile', $scope.profile).success(function(){
+	$location.path('/');	
+  });
+};
+});
