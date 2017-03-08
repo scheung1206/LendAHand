a94800b4a79d6ed361dd77b9fe20c684236410fc
brandon025 <brandon025@gmail.com>
2017-03-08 15:46:48 -0800

    added Chat Function and Facebook

--- client/app/chat/chat.controller.js
+++ client/app/chat/chat.controller.js
@@ -1,6 +1,26 @@
 'use strict';
 
-angular.module('codeApp')
-  .controller('ChatCtrl', function ($scope) {
-    $scope.message = 'Hello';
+angular.module("zoomchat", ["firebase"]);
+  .controller('ChatCtrl',  ['$scope','$firebaseArray',
+                function($scope, $firebaseArray) {
+                    var url = "https://lendachat-aeca6.firebaseio.com/"
+                    var keys = "tt";
+                    var res = url.concat(keys);
+                    
+                    var messages = new Firebase(res);
+                    $scope.messages = $firebaseArray(messages);
+
+                    $scope.newMessage = {from:"Me",message:""};
+
+                    $scope.sendMessage = function () {
+                        console.log("GOT IT!")
+                        $(document).ready(function(){
+                            $('#messagewindow').animate({
+                            scrollTop: $('#messagewindow')[0].scrollHeight}, 500);
+                        });
+                        
+                        
+                        $scope.messages.$add($scope.newMessage);
+                        $('#message').val("");
+                    }
   });
--- client/app/chat/chat.js
+++ client/app/chat/chat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('codeApp')
+angular.module('zoomchat')
   .config(function ($stateProvider) {
     $stateProvider
       .state('chat', {
