399cc8df84c24736f3a9eb56dfb63c382635c72d
mikextran <mikextran@yahoo.com>
2017-03-08 16:43:37 -0800

    Chatoom URL Fixed

--- client/app/chat/chat.js
+++ client/app/chat/chat.js
@@ -4,7 +4,8 @@ angular.module('codeApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('chat', {
-        url: '/chat/:id',
+        //url: '/chat/:id',
+        url: '/chat',
         templateUrl: 'app/chat/chat.html',
         controller: 'ChatCtrl'
       });
